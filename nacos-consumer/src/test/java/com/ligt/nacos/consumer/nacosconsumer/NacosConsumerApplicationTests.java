9ebc089cf97d34f14e399d5901059df3e353cd2b
ligt <1627739846@qq.com>
2021-07-27 11:21:46 +0800

    nacos init

---
+++ nacos-consumer/src/test/java/com/ligt/nacos/consumer/nacosconsumer/NacosConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ligt.nacos.consumer.nacosconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
